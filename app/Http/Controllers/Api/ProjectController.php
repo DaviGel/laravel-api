f482ef41c613b1e81b09f1257e877a7483e1dce9
DaviGel <geloso.davide@gmail.com>
2024-02-16 07:48:10 +0100

    feat: aggiunta paginazione dei progetti

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,7 @@ class ProjectController extends Controller
     public function index()
     {
         // Salvo tutti i progetti nella variabile projects
-        $projects = Project::all();
+        $projects = Project::paginate(12);
         // Converto i progetti in un json che conterrà due chiavi: success (settato a true) e results (contentente tutti i progetti)
         return response()->json([
             'success' => true,
